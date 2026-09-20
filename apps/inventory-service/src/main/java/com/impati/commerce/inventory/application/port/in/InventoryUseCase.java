c3ae205fde1b5391a0b82f9e59ea9e3fc9e71723
impati <yongs170@naver.com>
2026-09-20 17:50:14 +0900

    refactor: 컨벤션 맞추기

--- apps/inventory-service/src/main/java/com/impati/commerce/inventory/application/port/in/InventoryUseCase.java
+++ apps/inventory-service/src/main/java/com/impati/commerce/inventory/application/port/in/InventoryUseCase.java
@@ -2,8 +2,11 @@ package com.impati.commerce.inventory.application.port.in;
 
 import java.util.List;
 
-/** 재고로 할 수 있는 일. */
+/**
+ * 재고로 할 수 있는 일.
+ */
 public interface InventoryUseCase {
+
     StockDetails addStock(String skuId, int quantity);
 
     ReservationDetails reserve(String orderId, List<StockLine> lines);
@@ -20,6 +23,8 @@ public interface InventoryUseCase {
 
     StockDetails getStock(String skuId);
 
-    /** 시드가 이미 들어가 있는지 확인한다. 파일 DB에서는 재시작마다 넣으면 재고가 늘어난다. */
+    /**
+     * 시드가 이미 들어가 있는지 확인한다. 파일 DB에서는 재시작마다 넣으면 재고가 늘어난다.
+     */
     boolean isEmpty();
 }
